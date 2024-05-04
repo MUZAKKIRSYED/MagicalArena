207670e7524d8307ca2eab806e616ec2e2cecc67
muzakkir <muzakkirsyed123@gmail.com>
2024-05-04 16:41:38 +0530

    Improved Code Readability

--- src/main/java/com/logic/Logic.java
+++ src/main/java/com/logic/Logic.java
@@ -2,99 +2,96 @@ package com.logic;
 
 import java.util.Random;
 import com.model.Player;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Logic {
 
-	public static String play(Player p1,Player p2) {
-		try{
+	public static String play(Player Player1, Player player2) {
+		try {
 			// Checking the values of Health to determine Winner
-			if(p1.getHealth()==0 && p2.getHealth()==0) {
+			if (Player1.getHealth() == 0 && player2.getHealth() == 0) {
 				return "No Health to start the game";
-			}
-			else if(p1.getHealth()==0) {
+			} else if (Player1.getHealth() == 0) {
 				return "Player 2 wins";
-			}
-			else if(p2.getHealth()==0) {
+			} else if (player2.getHealth() == 0) {
 				return "Player 1 wins";
 			}
-			
-			//Checking the Values of Attack and Health
+
+			// Checking the Values of Attack and Health
 			List<Integer> list = new ArrayList<Integer>();
-			list.add(p1.getAttack());list.add(p1.getStrength());list.add(p2.getAttack());list.add(p2.getStrength());
-			int zeroCount=0;
-			for(int i=0;i<4;i++) {
-				if(list.get(i)==0) {
+			list.add(Player1.getAttack());
+			list.add(Player1.getStrength());
+			list.add(player2.getAttack());
+			list.add(player2.getStrength());
+			int zeroCount = 0;
+			for (int i = 0; i < 4; i++) {
+				if (list.get(i) == 0) {
 					zeroCount++;
 				}
 			}
-			
-			if(zeroCount>1) {
+
+			if (zeroCount > 1) {
 				return "Improper Attributes to Play";
 			}
-			
-			//Checking Negative Integers Condition and proceed with the Game 
-			if(p1.getHealth()>=0&&p1.getStrength()>=0&&p1.getAttack()>=0&&p2.getHealth()>=0&&p2.getStrength()>=0&&p2.getAttack()>=0) {
+
+			// Checking Negative Integers Condition and proceed with the Game
+			if (Player1.getHealth() >= 0 && Player1.getStrength() >= 0 && Player1.getAttack() >= 0
+					&& player2.getHealth() >= 0 && player2.getStrength() >= 0 && player2.getAttack() >= 0) {
 				String resultCheck = "continue";
 				boolean condition = true;
-				resultCheck = check(p1, p2);
-				if(p1.getHealth()>p2.getHealth()){
-					playing(p1, p2);
+				resultCheck = check(Player1, player2);
+				if (Player1.getHealth() > player2.getHealth()) {
+					playing(Player1, player2);
 					condition = false;
+				} else if (Player1.getHealth() < player2.getHealth()) {
+					playing(player2, Player1);
 				}
-				else if(p1.getHealth()<p2.getHealth()){
-					playing(p2, p1);
-				}
-				resultCheck = check(p1, p2);
-				while(resultCheck=="continue") {		
-					if(condition==true) {
-						playing(p1,p2);
+				resultCheck = check(Player1, player2);
+				while (resultCheck == "continue") {
+					if (condition == true) {
+						playing(Player1, player2);
 						System.out.println("p2 attack");
-						condition=false;
-					}
-					else {
-						playing(p2,p1);
+						condition = false;
+					} else {
+						playing(player2, Player1);
 						System.out.println("p1 attack");
-						condition=true;
+						condition = true;
 					}
-					resultCheck = check(p1, p2);
+					resultCheck = check(Player1, player2);
 				}
 				return resultCheck;
-			}
-			else {
+			} else {
 				return "Attributes can't be negative";
 			}
-		}
-		catch(Exception e) {
-			return ""+e;
+		} catch (Exception e) {
+			return "" + e;
 		}
 	}
-	
-	// Actual Method of Game Format
-	public static void playing(Player p1,Player p2) {
+
+	// Actual Method of Game Implementation
+	public static void playing(Player player1, Player player2) {
 		Random random = new Random();
-		int attackingDie = random.nextInt(6)+1;
-		int defendingDie = random.nextInt(6)+1;
-		int attackingScore = p2.getAttack()*attackingDie;
-		int defendingScore = p1.getStrength()*defendingDie;
-		if(attackingScore>defendingScore) {
-			int resultScore = p1.getHealth()-(attackingScore-defendingScore);
-			p1.setHealth(resultScore);
+		int attackingDie = random.nextInt(6) + 1;
+		int defendingDie = random.nextInt(6) + 1;
+		int attackingScore = player2.getAttack() * attackingDie;
+		int defendingScore = player1.getStrength() * defendingDie;
+		if (attackingScore > defendingScore) {
+			int resultScore = player1.getHealth() - (attackingScore - defendingScore);
+			player1.setHealth(resultScore);
 		}
-		System.out.println(attackingDie+" " +defendingDie+" "+attackingScore+" " +defendingScore+p1.getHealth()+" "+p2.getHealth());
+		System.out.println(attackingDie + " " + defendingDie + " " + attackingScore + " " + defendingScore
+				+ player1.getHealth() + " " + player2.getHealth());
 	}
-	
-	// Method to check the Status of Health of Players 
-	public static String check(Player p1,Player p2) {
-		if(p1.getHealth()<=0) {
+
+	// Method to check the Status of Health of Players
+	public static String check(Player player1, Player player2) {
+		if (player1.getHealth() <= 0) {
 			return "Player 2 wins";
-		}
-		else if(p2.getHealth()<=0) {
+		} else if (player2.getHealth() <= 0) {
 			return "Player 1 wins";
 		}
 		return "continue";
 	}
-	
+
 }
--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -3,22 +3,32 @@ package com.main;
 import com.logic.Logic;
 import com.model.Player;
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		try(Scanner scan = new Scanner(System.in)){
-			System.out.print("Enter Player 1 Health: ");int player1Health = scan.nextInt();
-			System.out.print("Enter Player 1 Strength: ");int player1Strength = scan.nextInt();
-			System.out.print("Enter Player 1 Attack: ");int player1Attack = scan.nextInt();
-			System.out.print("Enter Player 2 Health: ");int player2Health = scan.nextInt();
-			System.out.print("Enter Player 2 Strength: ");int player2Strength = scan.nextInt();
-			System.out.print("Enter Player 2 Attack: ");int player2Attack = scan.nextInt();
-			
-			//Initializing Player Objects 
-			Player p1 = new Player(player1Health,player1Strength,player1Attack);
-			Player p2 = new Player(player2Health,player2Strength,player2Attack);
-			System.out.println(Logic.play(p1,p2));
+
+		// Reading Input from user
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.print("Enter Player 1 Health: ");
+			int player1Health = scan.nextInt();
+			System.out.print("Enter Player 1 Strength: ");
+			int player1Strength = scan.nextInt();
+			System.out.print("Enter Player 1 Attack: ");
+			int player1Attack = scan.nextInt();
+			System.out.print("Enter Player 2 Health: ");
+			int player2Health = scan.nextInt();
+			System.out.print("Enter Player 2 Strength: ");
+			int player2Strength = scan.nextInt();
+			System.out.print("Enter Player 2 Attack: ");
+			int player2Attack = scan.nextInt();
+
+			// Initializing Player Objects
+			Player player1 = new Player(player1Health, player1Strength, player1Attack);
+			Player player2 = new Player(player2Health, player2Strength, player2Attack);
+			System.out.println(Logic.play(player1, player2));
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		catch(Exception e) {System.out.println(e);}
 	}
 }
