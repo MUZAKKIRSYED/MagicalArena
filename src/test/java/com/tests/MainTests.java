eed9ce77aa49ad2004deecb8e00b398b2150d9bc
muzakkir <muzakkirsyed123@gmail.com>
2024-05-04 17:01:49 +0530

    Organized Unit tests

--- src/test/java/com/tests/MainTests.java
+++ src/test/java/com/tests/MainTests.java
@@ -11,22 +11,27 @@ import com.model.Player;
 public class MainTests {
 
 	@Test
-	public void testsForStartMethod() {
-		List<String> resultList = new ArrayList<String>();
-		resultList.add("Player 1 wins");
-		resultList.add("Player 2 wins");
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(0,0,0),new Player(0,0,0)));
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(0,10,0),new Player(0,10,0)));
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(0,10,0),new Player(10,10,0)));
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(100,0,0),new Player(10,0,0)));
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(0,10,10),new Player(0,10,10)));
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(100,0,0),new Player(10,10,0)));
-		assertEquals("Incorrect Attributes to Play",Logic.play(new Player(100,10,0),new Player(10,10,0)));
-		assertTrue(resultList.contains(Logic.play(new Player(50,5,10),new Player(1000,10,5))));
-		assertTrue(resultList.contains(Logic.play(new Player(50,5,10),new Player(1000,10,5))));
-		assertTrue(resultList.contains(Logic.play(new Player(50,5,10),new Player(1000,10,5))));
-		assertTrue(resultList.contains(Logic.play(new Player(50,5,10),new Player(1000,10,5))));
-		assertTrue(resultList.contains(Logic.play(new Player(50,5,10),new Player(1000,10,5))));
-		assertEquals("Attributes can't be negative",Logic.play(new Player(-50,5,10),new Player(100,10,5)));
+	public void testsForZeroHealth() {
+		assertEquals("No Health to start the game",Logic.play(new Player(0,0,0),new Player(0,0,0)));
+		assertEquals("No Health to start the game",Logic.play(new Player(0,10,0),new Player(0,10,0)));
+		assertEquals("Player 2 wins",Logic.play(new Player(0,10,0),new Player(10,10,0)));
+		assertEquals("Player 1 wins",Logic.play(new Player(10,10,0),new Player(0,10,0)));
+		assertEquals("No Health to start the game",Logic.play(new Player(0,10,10),new Player(0,10,10)));
+		assertEquals("No Health to start the game",Logic.play(new Player(0,0,10),new Player(0,0,10)));
+		assertEquals("No Health to start the game",Logic.play(new Player(0,10,10),new Player(0,0,10)));
+		assertEquals("No Health to start the game",Logic.play(new Player(0,10,0),new Player(0,10,10)));
+	}
+	@Test
+	public void testForZeroValueAttributes() {
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,0,0),new Player(10,0,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,0,0),new Player(10,10,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,10,0),new Player(10,0,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,0,0),new Player(10,10,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,10,0),new Player(10,0,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,0,10),new Player(10,0,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,10,0),new Player(10,10,0)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,0,10),new Player(10,0,10)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,10,0),new Player(10,0,10)));
+		assertEquals("Improper Attributes to Play",Logic.play(new Player(100,0,10),new Player(10,10,0)));
 	}
 }
