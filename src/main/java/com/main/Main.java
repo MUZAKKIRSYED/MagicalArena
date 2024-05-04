011a8079a58bc4b25aa7a17d82b5d795eb4a2738
muzakkir <muzakkirsyed123@gmail.com>
2024-05-04 14:39:36 +0530

    Exception Handling Done

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -6,8 +6,29 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Player p1 = new Player(50,5,10);
-		Player p2 = new Player(100,10,5);
-		Logic.play(p1, p2);
+		try(Scanner scan = new Scanner(System.in)) {
+			//Taking Input
+			System.out.print("Enter Player 1 Health: ");int player1Health = scan.nextInt();
+			System.out.print("Enter Player 1 Strength: ");int player1Strength = scan.nextInt();
+			System.out.print("Enter Player 1 Attack: ");int player1Attack = scan.nextInt();
+			System.out.print("Enter Player 2 Health: ");int player2Health = scan.nextInt();
+			System.out.print("Enter Player 2 Strength: ");int player2Strength = scan.nextInt();
+			System.out.print("Enter Player 2 Attack: ");int player2Attack = scan.nextInt();
+			
+			//Initializing Player Objects 
+			Player p1 = new Player(player1Health,player1Strength,player1Attack);
+			Player p2 = new Player(player2Health,player2Strength,player2Attack);
+			 
+			//Checking Negative Integers Condition 
+			if(player1Health>=0&&player1Strength>=0&&player1Attack>=0&&player2Health>=0&&player2Strength>=0&&player2Attack>=0) {
+				Logic.play(p1, p2);
+			}
+			else {
+				System.out.println("Incorrect Attributes");
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 }
